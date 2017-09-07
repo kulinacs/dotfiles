4f5162607b39529bc1679f4ab34ca2fe5635fb27
Nicklaus McClendon <nicklaus@kulinacs.com>
2017-09-07 12:20:32 -0500

    Update emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -20,6 +20,10 @@
 	     :ensure t)
 (use-package yaml-mode
 	     :ensure t)
+(use-package jinja2-mode
+	     :ensure t)
+(use-package magit
+	     :ensure t)
 
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
@@ -55,3 +59,15 @@
 ;; Themes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'molokai t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (## yaml-mode markdown-mode flycheck use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
