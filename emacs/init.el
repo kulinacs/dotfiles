28e23297ef8ba5a6a89a9394f5934152b2635118
Nicklaus McClendon <nicklaus@kulinacs.com>
2017-11-30 14:41:45 -0600

    Remove dimish from emacs init.el

--- emacs/init.el
+++ emacs/init.el
@@ -11,7 +11,6 @@
 
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)
 (require 'bind-key)
 
 (use-package dockerfile-mode
@@ -70,7 +69,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (qml-mode ## yaml-mode markdown-mode flycheck use-package))))
+    (go-mode qml-mode ## yaml-mode markdown-mode flycheck use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
